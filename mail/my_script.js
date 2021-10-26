90e0597abc57958b20103dbd39d7865a1ff5afad
Michele Manco <87037954+MicheleManco@users.noreply.github.com>
2021-10-26 17:38:38 +0200

    impostato meglio

--- mail/my_script.js
+++ mail/my_script.js
@@ -3,17 +3,16 @@
 const listaMail = ["gionni@gmail.com", "sofia@gmail.com", "franco@gmail.com", "ernesta@gmail.com"];
 
 // chiedo all'utente la mail
-let mailUser = prompt("per verificare se sei in lista scrivi la tua mail:")
+const mailUser = prompt("per verificare se sei in lista scrivi la tua mail:")
 
 //controllo che sia in lista se è presente
-if (mailUser == listaMail[0]) {
-    alert("perfetto sei dei nostri!!!")
-} else if (mailUser == listaMail[1]){
-    alert("perfetto sei dei nostri!!!")
-} else if (mailUser == listaMail[2]){
-    alert("perfetto sei dei nostri!!!")
-} else if (mailUser == listaMail[3]){
-    alert("perfetto sei dei nostri!!!")
-} else {
-    alert("mi dispiace ma non sei in lista :(")
+let mailInLista = false; 
+for (let i = 0; i < listaMail.length; i++ ){
+    if (mailUser === listaMail[i]){
+        mailInLista = true;
+    }
+}
+
+if (mailInLista === true) {
+    
 }
