e62837c5cb9c9a7ec598286f863c6c4f213d83f6
Michele Manco <87037954+MicheleManco@users.noreply.github.com>
2021-10-26 18:02:30 +0200

    finito mail

--- mail/my_script.js
+++ mail/my_script.js
@@ -6,13 +6,17 @@ const listaMail = ["gionni@gmail.com", "sofia@gmail.com", "franco@gmail.com", "e
 const mailUser = prompt("per verificare se sei in lista scrivi la tua mail:")
 
 //controllo che sia in lista se è presente
+let trovata = document.getElementById("trovata")
+
 let mailInLista = false; 
+let posizione;
 for (let i = 0; i < listaMail.length; i++ ){
     if (mailUser === listaMail[i]){
         mailInLista = true;
+        posizione = i +1 
     }
 }
 
 if (mailInLista === true) {
-    
+    trovata.innerHTML = `La tua mail è nella lista, nella posizione ${posizione}`
 }
